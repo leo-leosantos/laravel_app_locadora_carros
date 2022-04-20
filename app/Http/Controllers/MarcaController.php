cb196135a1767c8f4578d74a1ea2076d1cd6f59c
Leandro Santos <lds.leosantos@gmail.com>
2022-04-20 15:37:40 -0300

    aula 300 - validações parte 4

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,79 +7,68 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+
     public function index()
     {
-        //
+        $marcas = $this->marca->all();
+        return response($marcas, 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+        $marca = $this->marca->create($request->all());
+        return response()->json($marca, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Marca $marca)
+
+    public function show($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+        return response()->json($marca, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Marca $marca)
     {
-        //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Marca $marca)
+
+    public function update(Request $request, $id)
     {
-        //
+
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Recurso nao localizado para atualização'], 404);
+        }
+
+        $request->validate($marca->rules(), $marca->feedback());
+
+        $marca->update($request->all());
+        return response()->json($marca, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Marca $marca)
+
+    public function destroy($id)
     {
-        //
+
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['erro' => 'Recurso nao localizado para remoção'], 404);
+        }
+        $marca->delete();
+        return response()->json(['msg' => 'A marca foi removida com sucesso'], 200);
     }
 }
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+    protected $fillable = ['nome', 'imagem'];
+
+
+    public function rules()
+    {
+        return  [
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required'
+        ];
+
+        /**
+         * unique
+         * 1) tabela
+         * 2) nome da coluna pesquisa na tabela
+         * 3) id do registro que sera desconsiderado na pesquisa
+         *
+         * @return void
+         */
+    }
+
+    public function feedback()
+    {
+        return  [
+            'required' => 'O campo :attribute é obrigatorio',
+            'nome.unique' => 'O nome da marca já existe',
+            'nome.min' => 'o nome deve ter no minino 3 caracteres'
+        ];
+    }
 }
