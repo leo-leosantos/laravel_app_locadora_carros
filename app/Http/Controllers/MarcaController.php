464399c6ce902977b2172f77bb5a2cec8647cd48
Leandro Santos <lds.leosantos@gmail.com>
2022-06-25 11:30:15 -0300

    Aula 319/320 - Repository Design Pattern -  Implementando um Repositoy para Marca e Modelo

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use App\Repositories\MarcaRepository;
 use Illuminate\Support\Facades\Storage;
+
 class MarcaController extends Controller
 {
 
@@ -15,33 +17,27 @@ class MarcaController extends Controller
 
     public function index(Request $request)
     {
-        $marcas = array();
+
+        $marcaRepository = new MarcaRepository($this->marca);
+
 
         if ($request->has('atributos_modelo')) {
-            $atributos_modelos = $request->atributos_modelo;
-            $marcas = $this->marca->with('modelos:id,' . $atributos_modelos);
+            $atributos_modelos = 'modelos:id,' . $request->atributos_modelo;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if ($request->has('filtro')) {
 
-            $filtros = explode(';', $request->filtro);
-
-            foreach ($filtros as $key => $condicao) {
-                $c = explode(':', $condicao);
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
         if ($request->has('atributos')) {
-
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($request->atributos);
         }
-        return response()->json($marcas, 200);
+
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
 
@@ -107,7 +103,7 @@ class MarcaController extends Controller
             $request->validate($marca->rules(), $marca->feedback());
         }
 
-        if($request->file('imagem')){
+        if ($request->file('imagem')) {
             Storage::disk('public')->delete($marca->imagem);
         }
 
