9221e410d31da2619b9516653aabee64fb77dc2b
Leandro Santos <lds.leosantos@gmail.com>
2022-06-28 16:47:30 -0300

    Aula 339 - Expirando o JWT por tempo limite

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,36 +9,39 @@ class AuthController extends Controller
     public function login(Request $request)
     {
 
-        $credenciais = $request->all(['email','password']);
+        $credenciais = $request->all(['email', 'password']);
 
-       // dd($credenciais);
+
+        // dd($credenciais);
         //authentication
 
         $token =  auth('api')->attempt($credenciais);
 
-        if($token){ //usuario autenticado com sucesso
-            return response()->json(['token'=>$token]);
-        }else{//erro de usuario ou senha
-            return response()->json(['erro'=>'Usuário ou senha invalido'], 403);
+        if ($token) { //usuario autenticado com sucesso
+            return response()->json(['token' => $token]);
+        } else { //erro de usuario ou senha
+            return response()->json(['erro' => 'Usuário ou senha invalido'], 403);
             //error 403 = forbidden -> proibido (login invalido)
             //error 401 = Unauthorized -> não autorizado
-
-
         }
 
         return 'login';
     }
     public function logout()
     {
-        return 'logout';
+        auth('api')->logout();
+        return response()->json(['msg'=>'Logout realizado com sucesso']);
+
     }
 
     public function refresh()
     {
-        return 'refresh';
+        $token = auth('api')->refresh();
+
+        return response()->json(['token' => $token]);
     }
     public function me()
     {
-        return 'me';
+        return response()->json(auth()->user());
     }
 }
